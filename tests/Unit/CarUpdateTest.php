74cd20dc197fd93efdb0b6c1779adea728f51f9c
nalandamagam <nalandachowdary1993@gmail.com>
2018-11-02 20:50:23 -0400

    Unit test for updating a car

---
+++ tests/Unit/CarUpdateTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Car;
+
+class CarUpdateTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $car = Car::find(1);
+        $car->year = 2000;
+        $this->assertTrue($car->save());
+    }
+}
